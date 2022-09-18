43901e3cbb91d330357be9c0aa623d1573a8dacb
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-18 15:14:52 +0900

    feat: create component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 // .vue ファイルをインポートすることでAppにオブジェクト化する（.vueファイルはそのまま使えないからコンポーネントのオブジェクトにする）
 import App from './App.vue'
+import LikeNumber from './LikeNumber.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('LikeNumber', LikeNumber);
 
 new Vue({
   render: h => h(App),
