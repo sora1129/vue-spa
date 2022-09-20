fcb881dfb87d027215cf6c74eb858c9676be7358
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-20 10:36:17 +0900

    feat: バリデーションを使用する

--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -7,7 +7,12 @@
 
 <script>
   export default {
-    props: ['totalNumber'],
+    props: {
+      totalNumber: {
+        type: Number,
+        default: 10
+      }
+    },
     computed: {
       halfNumber() {
         return this.totalNumber / 2
