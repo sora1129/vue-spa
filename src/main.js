f8b6379e77def1072214bd0bebc911841df357e4
sora1129 <yuka.izumisawa@sweeep.ai>
2022-10-13 11:27:08 +0900

    feat: フック関数について

--- src/main.js
+++ src/main.js
@@ -6,6 +6,25 @@ import LikeNumber from './components/LikeNumber.vue'
 Vue.config.productionTip = false
 
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive('border', {
+  bind() {
+    // ディレクティブが初めて対象の要素にひも付いた時
+    // 最初の1回に呼ばれるだけ
+  },
+  inserted() {
+    // 親Node(DOM)に挿入された時
+  },
+  update() {
+    // コンポーネントが更新される度。子コンポーネントが更新される前
+  },
+  componentUpdated() {
+    // コンポーネントが更新される度。子コンポーネントが更新された後
+  },
+  unbind() {
+    // ディレクティブがひも付いている要素から取り除かれた時
+    // コンポーネントにおけるdestroyedみたいな感じ
+  },
+});
 
 new Vue({
   render: h => h(App),
